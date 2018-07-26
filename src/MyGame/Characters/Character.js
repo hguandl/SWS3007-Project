262092fe4d5e57ec9f74991db28bda36741b5537
StephenNG59 <StephenNG59@users.noreply.github.com>
2018-07-26 15:40:02 +0800

    ?

--- src/MyGame/Characters/Character.js
+++ src/MyGame/Characters/Character.js
@@ -166,6 +166,8 @@ Character.prototype.incCurrentHP = function (delta) {
         this.mCurrentHP = this.mMaxHP;
     } else if (this.mCurrentHP + delta <= 0) {
         this.mCurrentHP = 0;
+    } else {
+        this.mCurrentHP += delta;
     }
 };
 Character.prototype.incMaxHP = function (delta) {
@@ -183,6 +185,8 @@ Character.prototype.incCurrentVP = function (delta) {
         this.mCurrentVP = this.mMaxVP;
     } else if (this.mCurrentVP + delta <= 0) {
         this.mCurrentVP = 0;
+    } else {
+        this.mCurrentVP += delta;
     }
 };
 Character.prototype.incMaxVP = function (delta) {
