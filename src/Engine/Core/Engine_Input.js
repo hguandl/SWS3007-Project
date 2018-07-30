5b765c382da91ff531203e7f9295c94f3445d970
hguandl <hguandl@gmail.com>
2018-07-31 00:06:58 +0800

    fix move issues

--- src/Engine/Core/Engine_Input.js
+++ src/Engine/Core/Engine_Input.js
@@ -187,23 +187,6 @@ gEngine.Input = (function () {
         return (mIsKeyReleased[keyCode]);
     };
 
-    var isDirectionLocked = function(keyCode) {
-        switch(keyCode) {
-            case kKeys.W:
-            return isKeyPressed(kKeys.A) || isKeyPressed(kKeys.S) || isKeyPressed(kKeys.D);
-            break;
-            case kKeys.S:
-            return isKeyPressed(kKeys.W) || isKeyPressed(kKeys.A) || isKeyPressed(kKeys.D);
-            break;
-            case kKeys.A:
-            return isKeyPressed(kKeys.W) || isKeyPressed(kKeys.S) || isKeyPressed(kKeys.D);
-            break;
-            case kKeys.D:
-            return isKeyPressed(kKeys.W) || isKeyPressed(kKeys.S) || isKeyPressed(kKeys.A);
-            break;
-        }
-    };
-
     var isButtonPressed = function (button) {
         return mIsButtonPressed[button];
     };
